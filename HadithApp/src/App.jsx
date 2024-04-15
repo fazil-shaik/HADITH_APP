a413a2b188d9b6fe1206006e31f3c56fdb1dddb9
Shaik Fazil <shaikfazil@Shaiks-MacBook-Air.local>
2024-04-15 10:11:24 +0530

    changes made in app.jsx and weak.css

--- HadithApp/src/App.jsx
+++ HadithApp/src/App.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from 'react';
 import love from './assets/love.svg';
+import './App.css'; 
 import './weak.css';
-import './App.css';
+
 function App() {
   const [data, setData] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true);
-
-
+  const [theme, setTheme] = useState('light'); // State for theme
 
   useEffect(() => {
     fetch('https://hadith-app-1.onrender.com/api/hadith')
       .then((response) => response.json())
-      .then((value) => setData(value))
-      setLoading(false);
-
+      .then((value) => {
+        setData(value);
+        setTimeout(() => {
+          setLoading(false); 
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false); 
+      });
   }, []);
 
   const handleNext = () => {
@@ -25,18 +32,26 @@ function App() {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? data.length - 1 : prevIndex - 1));
   };
 
-  return (
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light'); // Toggle theme
+  };
 
-    <div className="container">
-      <div className="top-left-icon">
-        <img src={love} width={30}></img>
-      </div>
-      <h1 className='title'>Sahi Al-Bhukhari Hadith</h1>
-      <div className="top-right-icon">
-        <img src={love} width={30}></img>
-      </div>
+  return (
+    <div className={`container ${theme === 'dark' ? 'dark-theme' : 'light-theme'}`}>
+       <div className="icon icon-left">
+    <img src={love} width={30} alt="Love Icon" />
+  </div>
+  <h1 className='title'>Sahi Al-Bhukhari Hadith</h1>
+  <div className="icon icon-right">
+    <img src={love} width={30} alt="Love Icon" />
+  </div>
+      <button onClick={toggleTheme} className="theme-toggle-button">
+        {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+      </button>
       <div className="hadith-container">
-        {data.length > 0 && (
+        {loading ? (
+          <p className='loader'></p> 
+        ) : data.length > 0 ? (
           <div>
             <p className='hadith-content'>{data[currentIndex].content}</p>
             <p className='hadith-reference'>{data[currentIndex].reference}</p>
@@ -45,11 +60,12 @@ function App() {
               <button onClick={handleNext} className='navigation-button'>Next Hadith</button>
             </div>
           </div>
+        ) : (
+          <p className='no-data'>No data available</p>
         )}
       </div>
     </div>
-
-  )
+  );
 }
 
 export default App;
